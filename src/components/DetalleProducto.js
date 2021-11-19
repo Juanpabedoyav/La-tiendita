8cd55180f810d1787ee91680d641965d8e33e87e
Cristian Guzman <cristiangt1813@gmail.com>
2021-11-19 18:39:46 -0500

    merge

--- src/components/DetalleProducto.js
+++ src/components/DetalleProducto.js
@@ -3,30 +3,12 @@ import {useParams} from 'react-router-dom';
 
 function DetalleProducto({oferta, popular}) {
     const {id} = useParams()
-    const [warning, setWarning] = useState([])
     const fOferta = oferta.find(el => el.nombre === id)
     const fPopular = popular.find(el => el.nombre === id)
-
-    useEffect(() => {
-        if (fOferta.nombre !== undefined) {
-            setWarning(fOferta.nombre);
-        } else {
-            alert('no')
-        }
-        // if (fPopular.nombre !== undefined) {
-        //     setWarning(fPopular.nombre);
-        // }
-        console.log(fOferta.nombre);
-        console.log(warning)
-    }, [])
-    useEffect(() => {
-        console.log(warning)
-    }, [warning])
+    
     return (
       <>
-        {/* <h1>{warning} :3</h1> */}
-        {/* <div>{fPopular.nombre && <h1>{fPopular.nombre}</h1>}</div> */}
-        {/* <div>{fPopular.nombre && <h1>{fPopular.nombre}</h1>}</div> */}
+        <div>{fPopular === undefined ? <h1>{fOferta.nombre}</h1> : <h1>{fPopular.nombre}</h1>}</div>
       </>
     );
 }
