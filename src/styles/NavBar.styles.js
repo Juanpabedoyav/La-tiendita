2316267d8fe6573c20f79ee644ab10b043a8b6d6
Cristian Guzman <cristiangt1813@gmail.com>
2021-11-19 09:56:27 -0500

    Subiendo el navBar

--- src/styles/NavBar.styles.js
+++ src/styles/NavBar.styles.js
@@ -1,1 +1,11 @@
 import styled from 'styled-components';
+
+export const NavBarStyles = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    align-items:center;
+    border: 1px solid black;
+    .titulo {
+        
+    }
+`
