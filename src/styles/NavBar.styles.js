1ef1ec0497ff974d7d5ef49a56c652d7be807b85
Santiago Montoya Cano <santiagomontoya98@hotmail.com>
2021-11-19 20:14:02 -0500

    La tiendita

--- src/styles/NavBar.styles.js
+++ src/styles/NavBar.styles.js
@@ -1,12 +1,10 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const NavBarStyles = styled.nav`
-    margin: 20px 0;
-    display: flex;
-    justify-content: space-between;
-    align-items:center;
-    border: 1px solid black;
-    .titulo {
-        
-    }
-`
+  margin: 20px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
