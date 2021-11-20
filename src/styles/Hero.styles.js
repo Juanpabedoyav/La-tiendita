b5543d20827353dea850318fadf51c7cc3e06ce7
Santiago Montoya Cano <santiagomontoya98@hotmail.com>
2021-11-19 20:38:06 -0500

    Tiendita

--- src/styles/Hero.styles.js
+++ src/styles/Hero.styles.js
@@ -14,7 +14,7 @@ export const HeroStyle = styled.div`
   @media (min-width: 786px) {
     background-image: url("https://res.cloudinary.com/workshop-principe/image/upload/v1637333891/banner_it0xng.png");
     height: 200px;
-    width: 100%;
+    width: 79.5vw;
   }
 `;
 export const HeroTitulo = styled.h2`
--- src/styles/Registro.styles.js
+++ src/styles/Registro.styles.js
@@ -1,19 +1,16 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const ButtonContainer = styled.div`
-margin-top: 30px;
-display: flex;
-justify-content: space-evenly;
-.boton{
+  margin-top: 30px;
+  display: flex;
+  justify-content: space-evenly;
+  .boton {
     background-color: #0d77b8;
-}
-
-`
-
+  }
+`;
 
 export const Formulario = styled.form`
-    width: 70vw;
-    margin-left: auto;
-    margin-right: auto;
-
+  width: 70vw;
+  margin-left: auto;
+  margin-right: auto;
 `;
